7c2b4dca080bcb7a8117ed85fabb5b17e190488a
SudoAttack <wayne8798@gmail.com>
2014-03-07 17:02:47 -0600

    now we throw away none ascii strings

--- backend/post_extractor.py
+++ backend/post_extractor.py
@@ -2,12 +2,18 @@ import praw
 
 def retrieve_posts(school_name):
     posts = r.get_subreddit(school_name).get_hot(limit=1000)
-    fout = open('data/' + school_name, 'w')
+    fout = open('data/'+school_name, 'w')
     for x in posts:
         score = int(str(x).split('::')[0])
-        text = str(x).split('::')[1]
-        for i in range(0, 1+score/10):
-            fout.write(text)
+        text = str(x).split('::')[1].lower()
+
+        # throw away none ascii strings
+        try:
+            text.decode('ascii')
+            for i in range(0, 1+score/10):
+                fout.write(text)
+        except UnicodeDecodeError:
+            print 'None ASCII: ' + text
     fout.close()
 
 r = praw.Reddit(user_agent="flagship-map by wayne8798")
@@ -16,4 +22,5 @@ for line in f.readlines():
     if line[0] != '#':
         school_name = line.split()[0]
         retrieve_posts(school_name)
+        print 'Finish fetching ' + school_name
 f.close()
